d934acc36952c4dbeb134ee9f9fa787adddcc3b5
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-11 16:43:24 +0000

    Tuesday Progress

---
+++ src/components/Calcs.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Calcs extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}

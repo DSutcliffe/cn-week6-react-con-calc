1fd084766f9f8841a72eff7a84219fdd79f4cb66
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-11 16:43:14 +0000

    Tuesday Progress

--- src/App.js
+++ src/App.js
@@ -1,55 +1,192 @@
-import React from 'react';
-
+import React, { Component } from 'react'
 import './css/App.css';
 
-function App() {
-  return (
-    <div>
+// import Calcs from "./components/Calcs";
 
-      <div class="line1">
-        <div class="display">
-          <h1>0</h1>
+export default class App extends Component {
+
+  state = {
+    displayNumber: 0,
+    storedNumber: "",
+    currentNumber: "",
+    operator: "",
+    sum: 0
+  }
+
+  recordEntry = (buttonPressed) => {
+    console.log(buttonPressed);
+    console.log(typeof(buttonPressed));
+
+    const stateCopy = {...this.state}
+
+    // if(stateCopy.displayNumber === 0) {
+    //   stateCopy.displayNumber = "";
+    // }
+    
+    // this.setState({
+    //   displayNumber: stateCopy.displayNumber + buttonPressed.toString()
+    // })
+
+    switch(buttonPressed) {
+      case "ac":
+        this.setState({
+          displayNumber: 0,
+          currentNumber: "",
+          storedNumber: ""
+        })
+        break;
+      case 0:
+        if(stateCopy.displayNumber === 0) {
+          this.setState({
+            displayNumber: 0,
+            currentNumber: "",
+            storedNumber: ""
+          })
+        } else {
+          this.setState({
+            displayNumber: stateCopy.displayNumber + buttonPressed.toString(),
+            currentNumber: stateCopy.displayNumber + buttonPressed.toString(),
+            storedNumber: stateCopy.displayNumber + buttonPressed.toString()
+          })
+        }
+        break;
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+        if(stateCopy.displayNumber === 0) {
+          stateCopy.displayNumber = "";
+          this.setState({
+            displayNumber: stateCopy.displayNumber + buttonPressed.toString(),
+            currentNumber: stateCopy.displayNumber + buttonPressed.toString(),
+            storedNumber: stateCopy.displayNumber + buttonPressed.toString()
+          })
+        } else {
+          this.setState({
+            displayNumber: stateCopy.displayNumber + buttonPressed.toString(),
+            currentNumber: stateCopy.displayNumber + buttonPressed.toString(),
+            storedNumber: stateCopy.displayNumber + buttonPressed.toString()
+          })
+        }
+        break;
+      case "÷":
+        console.log("IN DIVIDE CASE")
+    
+        break;
+      case "x":
+        console.log("IN MULTIPLY CASE")
+   
+        break;
+      case "-":
+        console.log("IN MINUS CASE")
+     
+        break;
+      case "+":
+        console.log("IN ADDITION CASE")
+
+        if (this.state.currentNumber > 0) {
+          this.setState({
+            operator: "+"
+          })
+          console.log("OPERATORE SET TO +")
+        }
+       
+        break;
+      case "=":
+        console.log("IN EQUAL CASE")
+        if (this.state.operator === "+") {
+          this.setState({
+            sum: this.state.currentNumber + this.state.displayNumber
+          })
+        } 
+        // else if (this.state.operator === "-") {
+        //   this.setState({
+        //     sum: this.state.currentNumber - this.state.displayNumber
+        //   })
+        // } 
+        // else if (this.state.operator === "x") {
+        //   this.setState({
+        //     sum: this.state.currentNumber * this.state.displayNumber
+        //   })
+        // } 
+        // else if (this.state.operator === "÷") {
+        //   this.setState({
+        //     sum: this.state.currentNumber / this.state.displayNumber
+        //   })
+        // }
+       
+        break;
+      case "+/-":
+        console.log("Function Not Yet Supported")
+        break;
+      case "%":
+        console.log("Function Not Yet Supported")
+        break;
+      case ".":
+        console.log("Function Not Yet Supported")
+        break;
+      default:
+        console.log("Calulator Failed!")
+    }
+    
+  }
+
+  render() {
+
+    console.log(`Stored Number: ${this.state.currentNumber}`)
+    console.log(`Display Number: ${this.state.displayNumber}`)
+
+    return (
+      <div>
+
+      <div className="line1">
+        <div className="display">
+          <h1>{this.state.displayNumber}</h1>
         </div>
       </div>
 
-      <div class="line2">
-        <button class="buttonFuncA AC">AC</button>
-        <button class="buttonFuncA plusMinus">+/-</button>
-        <button class="buttonFuncA percent">%</button>
-        <button class="buttonFuncB divide">÷</button>
+      <div className="line2">
+        <button onClick={() => this.recordEntry("ac")} id="ac" className="buttonFuncA AC">AC</button>
+        <button onClick={() => this.recordEntry("+/-")} id="+/-" className="buttonFuncA plusMinus">+/-</button>
+        <button onClick={() => this.recordEntry("%")} id="%" className="buttonFuncA percent">%</button>
+        <button onClick={() => this.recordEntry("÷")} id="÷" className="buttonFuncB divide">÷</button>
       </div>
 
-      <div class="line3">
-        <button class="button seven">7</button>
-        <button class="button eight">8</button>
-        <button class="button nine">9</button>
-        <button class="buttonFuncB multiply">x</button>
+      <div className="line3">
+        <button onClick={() => this.recordEntry(7)} className="button seven">7</button>
+        <button onClick={() => this.recordEntry(8)} className="button eight">8</button>
+        <button onClick={() => this.recordEntry(9)} id="9" className="button nine">9</button>
+        <button onClick={() => this.recordEntry("x")} id="x" className="buttonFuncB multiply">x</button>
       </div>
 
-      <div class="line4">
-        <button class="button four">4</button>
-        <button class="button five">5</button>
-        <button class="button six">6</button>
-        <button class="buttonFuncB minus">-</button>
+      <div className="line4">
+        <button onClick={() => this.recordEntry(4)} id="4" className="button four">4</button>
+        <button onClick={() => this.recordEntry(5)} id="5" className="button five">5</button>
+        <button onClick={() => this.recordEntry(6)} id="6" className="button six">6</button>
+        <button onClick={() => this.recordEntry("-")} id="-" className="buttonFuncB minus">-</button>
       </div>
 
-      <div class="line5">
-        <button class="button four">1</button>
-        <button class="button five">2</button>
-        <button class="button six">3</button>
-        <button class="buttonFuncB plus">+</button>
+      <div className="line5">
+        <button onClick={() => this.recordEntry(1)} id="1" className="button four">1</button>
+        <button onClick={() => this.recordEntry(2)} id="2" className="button five">2</button>
+        <button onClick={() => this.recordEntry(3)} id="3" className="button six">3</button>
+        <button onClick={() => this.recordEntry("+")} id="+" className="buttonFuncB plus">+</button>
       </div>
 
-      <div class="line6">
-        <button class="button zero">0</button>
+      <div className="line6">
+        <button onClick={() => this.recordEntry(0)} id="0" className="button zero">0</button>
   
-        <button class="button point">.</button>
-        <button class="buttonFuncB equal">=</button>
+        <button onClick={() => this.recordEntry(".")} id="." className="button point">.</button>
+        <button onClick={() => this.recordEntry("=")} id="=" className="buttonFuncB equal">=</button>
         
       </div>
       
     </div>
-  );
+    )
+  }
 }
-
-export default App;

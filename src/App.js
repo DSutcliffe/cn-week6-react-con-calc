e9486270b06757c491aaac6a0a5d01b544acdcf8
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-12 15:02:51 +0000

    Not Using = Function

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ export default class App extends Component {
     firstNumber: "",
     operator: "",
     currentNumber: 0,
-    firstNumberStored: false
+    firstNumberStored: false,
+    newNumber: true
   }
 
   acSelect = () => {
@@ -26,13 +27,25 @@ export default class App extends Component {
       stateCopy.displayNumber = "";
       this.setState({
         displayNumber: stateCopy.displayNumber + buttonPressed.toString(),
-        currentNumber: stateCopy.displayNumber + buttonPressed.toString()
+        currentNumber: stateCopy.displayNumber + buttonPressed.toString(),
+        newNumber: false
       })
     } else {
-      this.setState({
+      if(this.state.newNumber){
+        stateCopy.displayNumber = "";
+        this.setState({
         displayNumber: stateCopy.displayNumber + buttonPressed.toString(),
-        currentNumber: stateCopy.displayNumber + buttonPressed.toString()
+        currentNumber: stateCopy.displayNumber + buttonPressed.toString(),
+        newNumber: false
       })
+      } else {
+        this.setState({
+          displayNumber: stateCopy.displayNumber + buttonPressed.toString(),
+          currentNumber: stateCopy.displayNumber + buttonPressed.toString(),
+          newNumber: false
+        })
+      }
+      
     }
   }
 
@@ -43,13 +56,14 @@ export default class App extends Component {
       firstNumber: "",
       operator: "",
       currentNumber: 0,
-      firstNumberStored: false
+      firstNumberStored: false,
+      newNumber: true
     })
 
   }
 
-  performCalc = (operator) => {
-    console.log("IN PERFORM CALC")
+  operatorSelect = (operator) => {
+    console.log("IN OPERATORSELECT FUNCTION")
     switch(operator){
       case "+":
         this.addition()
@@ -76,19 +90,33 @@ export default class App extends Component {
     console.log("IN ADDITION FUNCTION")
     const stateCopy = {...this.state}
 
+    console.log("Store Operator")
     this.setState({
       operator: "+"
     })
 
-    if(stateCopy.firstNumberStored) {
+    // firstNumberStored initially set to false
+    // if(stateCopy.firstNumberStored) {
+    if(stateCopy.firstNumberStored && stateCopy.currentNumber === 0) {
       console.log("Plus pressed before equal!")
+    } else if (stateCopy.firstNumberStored && stateCopy.currentNumber > 0) {
+      console.log("firstNumber and current number > 0")
+
+      this.setState({
+        displayNumber: parseInt(this.state.firstNumber) + parseInt(this.state.currentNumber),
+        currentNumber: parseInt(this.state.firstNumber) + parseInt(this.state.currentNumber),
+        firstNumber: parseInt(this.state.firstNumber) + parseInt(this.state.currentNumber),
+        newNumber: true
+      })
+
     } else {
       console.log("Store currentNumber in firstNumber")
+      console.log("Reset currentNumber to 0")
       this.setState({
         firstNumber: stateCopy.currentNumber,
-        displayNumber: 0,
         currentNumber: 0,
-        firstNumberStored: true
+        firstNumberStored: true,
+        newNumber: true
       })
     }
 
@@ -118,7 +146,10 @@ export default class App extends Component {
       if(this.state.operator === "+") {
         this.setState({
           displayNumber: parseInt(this.state.firstNumber) + parseInt(this.state.currentNumber),
-          currentNumber: parseInt(this.state.firstNumber) + parseInt(this.state.currentNumber)
+          // currentNumber: parseInt(this.state.firstNumber) + parseInt(this.state.currentNumber),
+          firstNumber: parseInt(this.state.firstNumber) + parseInt(this.state.currentNumber),
+          operator: "",
+          newNumber: true
         })
       }
     } else {
@@ -130,8 +161,10 @@ export default class App extends Component {
   render() {
 
     console.log(`Display Number: ${this.state.displayNumber}`)
+    console.log(`Current Number: ${this.state.currentNumber}`)
     console.log(`First Number: ${this.state.firstNumber}`)
     console.log(`Operator: ${this.state.operator}`)
+    console.log(`First Number Stored: ${this.state.firstNumberStored}`)
 
     return (
       <div>
@@ -146,35 +179,35 @@ export default class App extends Component {
         <button onClick={() => this.acSelect()} className="buttonFuncA AC">AC</button>
         <button className="buttonFuncA plusMinus">+/-</button>
         <button className="buttonFuncA percent">%</button>
-        <button onClick={() => this.performCalc("÷")} className="buttonFuncB divide">÷</button>
+        <button onClick={() => this.operatorSelect("÷")} className="buttonFuncB divide">÷</button>
       </div>
 
       <div className="line3">
         <button onClick={() => this.numberSelect(7)} className="button seven">7</button>
         <button onClick={() => this.numberSelect(8)} className="button eight">8</button>
         <button onClick={() => this.numberSelect(9)} className="button nine">9</button>
-        <button onClick={() => this.performCalc("x")} className="buttonFuncB multiply">x</button>
+        <button onClick={() => this.operatorSelect("x")} className="buttonFuncB multiply">x</button>
       </div>
 
       <div className="line4">
         <button onClick={() => this.numberSelect(4)} className="button four">4</button>
         <button onClick={() => this.numberSelect(5)} className="button five">5</button>
         <button onClick={() => this.numberSelect(6)} className="button six">6</button>
-        <button onClick={() => this.performCalc("-")} className="buttonFuncB minus">-</button>
+        <button onClick={() => this.operatorSelect("-")} className="buttonFuncB minus">-</button>
       </div>
 
       <div className="line5">
         <button onClick={() => this.numberSelect(1)} className="button four">1</button>
         <button onClick={() => this.numberSelect(2)} className="button five">2</button>
         <button onClick={() => this.numberSelect(3)} className="button six">3</button>
-        <button onClick={() => this.performCalc("+")} className="buttonFuncB plus">+</button>
+        <button onClick={() => this.operatorSelect("+")} className="buttonFuncB plus">+</button>
       </div>
 
       <div className="line6">
         <button onClick={() => this.numberSelect(0)} className="button zero">0</button>
   
         <button className="button point">.</button>
-        <button onClick={() => this.performCalc("=")} className="buttonFuncB equal">=</button>
+        <button onClick={() => this.operatorSelect("=")} className="buttonFuncB equal">=</button>
         
       </div>
       

1eac6a99c1272feeb21838e9d80fc2c1bad0194c
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-12 11:50:37 +0000

    ADDITION WORKING

--- src/App.js
+++ src/App.js
@@ -7,144 +7,150 @@ export default class App extends Component {
 
   state = {
     displayNumber: 0,
-    currentNumber: "",
     firstNumber: "",
     secondNumber: "",
     operator: "",
-    sum: 0
+    sum: 0,
+    currentNumber: 0,
+    firstNumberStored: false
   }
 
-  recordEntry = (buttonPressed) => {
-    console.log(buttonPressed);
+  acSelect = () => {
+    this.newNumberStart()
+  }
 
+  numberSelect = (buttonPressed) => {
     const stateCopy = {...this.state}
-    
-    switch(buttonPressed) {
-      case "ac":
-        this.newNumberStart()
-        break;
 
-      case 0:
-        if(stateCopy.displayNumber === 0) {
-          this.newNumberStart()
-        } else {
-          this.setCurrentNumber(stateCopy.displayNumber, buttonPressed.toString())
-        }
-        break;
-
-      case 1:
-      case 2:
-      case 3:
-      case 4:
-      case 5:
-      case 6:
-      case 7:
-      case 8:
-      case 9:
-        if(stateCopy.displayNumber === 0) {
-          stateCopy.displayNumber = "";
-          this.setCurrentNumber(stateCopy.displayNumber, buttonPressed.toString())
-        } else {
-          this.setCurrentNumber(stateCopy.displayNumber, buttonPressed.toString())
-        }
-        break;
-
-      case "÷":
-        console.log("IN DIVIDE CASE")
-    
-        break;
-
-      case "x":
-        console.log("IN MULTIPLY CASE")
-   
-        break;
-
-      case "-":
-        console.log("IN MINUS CASE")
-     
-        break;
-
-      case "+":
-        console.log("IN ADDITION CASE")
-        
-        if (stateCopy.displayNumber > 0) {
-          this.addition(stateCopy.displayNumber)
-        }
-       
-        break;
-
-      case "=":
-        console.log("IN EQUAL CASE")
-        this.setState({
-          secondNumber: stateCopy.displayNumber
-        })
-        this.equals()
-       
-        break;
-
-      case "+/-":
-        console.log("Function Not Yet Supported")
-        break;
-
-      case "%":
-        console.log("Function Not Yet Supported")
-        break;
-
-      case ".":
-        console.log("Function Not Yet Supported")
-        break;
-
-      default:
-        console.log("Calulator Failed!")
-    }
-    
-  }
-
-  newNumberStart = () => {
-    console.log("TRYING TO RESET TO 0")
-    this.setState({
-      displayNumber: 0,
-      currentNumber: "",
-      firstNumber: 0,
-      secondNumber: 0,
-      sum: 0
-    })
-    console.log(`NewNumberStart ${this.state.displayNumber}`)
-  }
-
-  setCurrentNumber = (displayNo, buttonPressed) => {
-    this.setState({
-      displayNumber: displayNo + buttonPressed
-    })
-  }
-
-  addition = (displayNo) => {
-    if (this.state.firstNumber === "") {
+    if(stateCopy.displayNumber === 0 && buttonPressed === 0) {
+      this.newNumberStart()
+    } else if(stateCopy.displayNumber === 0) {
+      stateCopy.displayNumber = "";
       this.setState({
-        operator: "+",
-        firstNumber: displayNo
+        displayNumber: stateCopy.displayNumber + buttonPressed.toString(),
+        currentNumber: stateCopy.displayNumber + buttonPressed.toString()
       })
     } else {
       this.setState({
-        operator: "+",
-        secondNumber: displayNo
+        displayNumber: stateCopy.displayNumber + buttonPressed.toString(),
+        currentNumber: stateCopy.displayNumber + buttonPressed.toString()
       })
     }
   }
 
-  equals = () => {
-    console.log(`equals secondNumber ${this.state.secondNumber}`)
-    if(this.state.operator === "+") {
+  newNumberStart = () => {
+    console.log("RESET INITIAL VALUES")
+    this.setState({
+      displayNumber: 0,
+      firstNumber: "",
+      secondNumber: "",
+      operator: "",
+      sum: 0,
+      currentNumber: 0,
+      firstNumberStored: false
+    })
+
+  }
+
+  performCalc = (operator) => {
+    console.log("IN PERFORM CALC")
+    switch(operator){
+      case "+":
+        this.addition()
+        break;
+      case "-":
+        this.subtraction()
+        break;
+      case "x":
+        this.multiply()
+        break;
+      case "÷":
+        this.divide()
+        break;
+      case "=":
+        this.equals()
+        break;
+      default:
+        console.log("Something Went Horribly Wrong")
+    }
+
+  }
+
+  addition = () => {
+    console.log("IN ADDITION FUNCTION")
+    const stateCopy = {...this.state}
+
+    this.setState({
+      operator: "+"
+    })
+
+    if(stateCopy.firstNumberStored) {
+      console.log("PLUS PRESSED BEFORE EQUALS!")
+    } else {
+      console.log("STORE DISPLAY IN FIRSTNUMBER")
       this.setState({
-        sum: parseInt(this.state.firstNumber) + parseInt(this.state.secondNumber)
+        // firstNumber: stateCopy.displayNumber,
+        firstNumber: stateCopy.currentNumber,
+        displayNumber: 0,
+        currentNumber: 0,
+        firstNumberStored: true
       })
     }
+
   }
+  
+  subtraction = () => {
+    console.log("IN SUBTRACTION FUNCTION")
+  
+  }
+
+  multiply = () => {
+    console.log("IN MULTIPLY FUNCTION")
+  
+  }
+
+  divide = () => {
+    console.log("IN DIVIDE FUNCTION")
+  
+  }
+
+  equals = () => {
+    console.log("IN EQUALS FUNCTION")
+    const stateCopy = {...this.state}
+
+    if(stateCopy.firstNumberStored) {
+      console.log("firstNumber Has Value")
+      this.setState({
+        secondNumber: stateCopy.currentNumber
+      })
+      this.displayResult()
+    } else {
+      console.log("firstNumber has not been stored yet")
+    }
+
+  }
+
+  displayResult = () => {
+    console.log("IN DISPLAY RESULT FUNCTION")
+
+    if(this.state.operator === "+") {
+      this.setState({
+        displayNumber: parseInt(this.state.firstNumber) + parseInt(this.state.currentNumber)
+      })
+      // this.replaceFirstNum()
+    }
+    
+  }
+
+  // replaceFirstNum = () => {
+  //   this.setState({
+  //     firstNumber: this.state.displayNumber
+  //   })
+  // }
 
   render() {
 
     console.log(`Display Number: ${this.state.displayNumber}`)
-    console.log(`Current Number: ${this.state.currentNumber}`)
     console.log(`First Number: ${this.state.firstNumber}`)
     console.log(`Operator: ${this.state.operator}`)
     console.log(`Second Number: ${this.state.secondNumber}`)
@@ -160,38 +166,38 @@ export default class App extends Component {
       </div>
 
       <div className="line2">
-        <button onClick={() => this.recordEntry("ac")} className="buttonFuncA AC">AC</button>
-        <button onClick={() => this.recordEntry("+/-")} className="buttonFuncA plusMinus">+/-</button>
-        <button onClick={() => this.recordEntry("%")} className="buttonFuncA percent">%</button>
-        <button onClick={() => this.recordEntry("÷")} className="buttonFuncB divide">÷</button>
+        <button onClick={() => this.acSelect()} className="buttonFuncA AC">AC</button>
+        <button className="buttonFuncA plusMinus">+/-</button>
+        <button className="buttonFuncA percent">%</button>
+        <button onClick={() => this.performCalc("÷")} className="buttonFuncB divide">÷</button>
       </div>
 
       <div className="line3">
-        <button onClick={() => this.recordEntry(7)} className="button seven">7</button>
-        <button onClick={() => this.recordEntry(8)} className="button eight">8</button>
-        <button onClick={() => this.recordEntry(9)} className="button nine">9</button>
-        <button onClick={() => this.recordEntry("x")} className="buttonFuncB multiply">x</button>
+        <button onClick={() => this.numberSelect(7)} className="button seven">7</button>
+        <button onClick={() => this.numberSelect(8)} className="button eight">8</button>
+        <button onClick={() => this.numberSelect(9)} className="button nine">9</button>
+        <button onClick={() => this.performCalc("x")} className="buttonFuncB multiply">x</button>
       </div>
 
       <div className="line4">
-        <button onClick={() => this.recordEntry(4)} className="button four">4</button>
-        <button onClick={() => this.recordEntry(5)} className="button five">5</button>
-        <button onClick={() => this.recordEntry(6)} className="button six">6</button>
-        <button onClick={() => this.recordEntry("-")} className="buttonFuncB minus">-</button>
+        <button onClick={() => this.numberSelect(4)} className="button four">4</button>
+        <button onClick={() => this.numberSelect(5)} className="button five">5</button>
+        <button onClick={() => this.numberSelect(6)} className="button six">6</button>
+        <button onClick={() => this.performCalc("-")} className="buttonFuncB minus">-</button>
       </div>
 
       <div className="line5">
-        <button onClick={() => this.recordEntry(1)} className="button four">1</button>
-        <button onClick={() => this.recordEntry(2)} className="button five">2</button>
-        <button onClick={() => this.recordEntry(3)} className="button six">3</button>
-        <button onClick={() => this.recordEntry("+")} className="buttonFuncB plus">+</button>
+        <button onClick={() => this.numberSelect(1)} className="button four">1</button>
+        <button onClick={() => this.numberSelect(2)} className="button five">2</button>
+        <button onClick={() => this.numberSelect(3)} className="button six">3</button>
+        <button onClick={() => this.performCalc("+")} className="buttonFuncB plus">+</button>
       </div>
 
       <div className="line6">
-        <button onClick={() => this.recordEntry(0)} className="button zero">0</button>
+        <button onClick={() => this.numberSelect(0)} className="button zero">0</button>
   
-        <button onClick={() => this.recordEntry(".")} className="button point">.</button>
-        <button onClick={() => this.recordEntry("=")} className="buttonFuncB equal">=</button>
+        <button className="button point">.</button>
+        <button onClick={() => this.performCalc("=")} className="buttonFuncB equal">=</button>
         
       </div>
       
